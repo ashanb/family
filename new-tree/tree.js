1a7f2081556a7f6c0d0741c318e79150dc1bbe61
Ashan@Home <ashanb58@gmail.com>
2022-02-06 19:41:15 +0530

    no message

--- new-tree/tree.js
+++ new-tree/tree.js
@@ -1,4 +1,4 @@
-//JavaScript
+// JavaScript
 var family = new FamilyTree(document.getElementById("tree"), {
     mode: 'dark',
     mouseScrool: FamilyTree.none,
@@ -7,33 +7,32 @@ var family = new FamilyTree(document.getElementById("tree"), {
     }
 });
 
-// family.load([
-//     { id: 1, pids: [2], name: "Amber McKenzie", gender: "female" },
-//     { id: 2, pids: [1], name: "Ava Field", gender: "male" },
-//     { id: 3, mid: 1, fid: 2, name: "Peter Stevens", gender: "male" }
-// ]);
 
 // family.load([
-//     { "id": 1, "pids": [
-//         2
-//     ], "name": "Amber McKenzie", "gender": "female"
-//     },
-//     { "id": 2, "pids": [
-//         1
-//     ], "name": "Ava Field", "gender": "male"
-//     },
-//     { "id": 3, "mid": 1, "fid": 2, "name": "Peter Stevens", "gender": "male"
-//     }
-// ]);
+//             { id: 1, pids: [2], name: "Amber McKenzie", gender: "female" },
+//             { id: 2, pids: [1], name: "Ava Field", gender: "male" },
+//             { id: 3, mid: 1, fid: 2, name: "Peter Stevens", gender: "male" }
+//         ]);
 
 var arrItems = []; // THE ARRAY TO STORE JSON ITEMS.
+// console.log("Started");
+// $(document).ready(function () {
+//     console.log("Called");
+//     $.getJSON("./dataArray.json", function (data) {
+//         $.each(data, function (index, value) {
+//             console.log("Conatins Data, value");
+//             arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
+//             console.log(arrItems[0]);
+//         });
+//     });
+// });
 
-$(document).ready(function () {
-    $.getJSON("./dataArray.json", function (data) {  
-        $.each(data, function (index, value) {
-            arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
-        });
+$.getJSON("./dataArray.json", function (data) {
+    $.each(data, function (index, value) {
+        arrItems.push(value);       // PUSH THE VALUES INSIDE THE ARRAY.
+        // console.log(arrItems[0]);
     });
+    family.load(arrItems);
 });
 
-family.load(arrItems);
+// console.log("End");
